3eb963c0ad7a5cb28ed4669fa1b8d91aea0b94db
David Gethings <dgethings@juniper.net>
2017-06-14 09:29:56 +0100

    Use function to determine presence of ssh-agent

--- conf.d/init_ssh_agent.fish
+++ conf.d/init_ssh_agent.fish
@@ -1,5 +1,13 @@
-set ssh_agent_pid (pgrep ssh-agent)
-if $ssh_agent_pid[0]
+function agent_is_present -d "find ssh-agent process if one is running"
+    set ssh_agent_pid (pgrep ssh-agent)
+    if $ssh_agent_pid[0] > 1
+        return true
+    else
+        return false
+    end
+end
+
+if agent_is_present
     __test_ssh_identities
 else
     __start_ssh_agent
